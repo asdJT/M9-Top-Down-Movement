3cc56cd5afb4bf766fff8db821be2e58a7d1df0b
asdJT <123706939+asdJT@users.noreply.github.com>
2025-10-27 15:50:18 -0700

    M9L8

--- M09.top_down_movement_workbook/lessons/runner.gd
+++ M09.top_down_movement_workbook/lessons/runner.gd
@@ -8,6 +8,8 @@ extends CharacterBody2D
 @onready var _runner_visual: RunnerVisual = %RunnerVisualRed
 @onready var _dust: GPUParticles2D = %Dust
 
+signal walked_to
+
 func _physics_process(delta: float) -> void :
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	
@@ -34,3 +36,18 @@ func _physics_process(delta: float) -> void :
 	
 	
 	move_and_slide()
+
+func walk_to (destination_global_position: Vector2) -> void:
+	var direction := global_position.direction_to(destination_global_position)
+	_runner_visual.angle = direction.orthogonal().angle()
+	_runner_visual.animation_name = RunnerVisual.Animations.WALK
+	_dust.emitting = true
+	var distance := global_position.distance_to(destination_global_position)
+	var duration := distance/ (max_speed * 0.2)
+	var tween := create_tween()
+	tween.tween_property(self, "global_position", destination_global_position, duration)
+	tween.finished.connect(func ():
+		_runner_visual.animation_name = RunnerVisual.Animations.IDLE
+		_dust.emitting = false
+		walked_to.emit()
+	)
